977ccb511678bc72ed225a62e721d21a4404ddce
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-06 14:05:33 -0300

    feat: create an installation script

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+task_management_folder="/usr/local/share/task-management/"
+username=$SUDO_USER
+desktop_file_icon="/home/$username/.local/share/applications"
+echo "$desktop_file_icon"
+
+# Check for sudo privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "please run as sudo or use root"
+  exit 1
+fi
+
+# install dependencies
+# apt update
+if ! command -v python3 &>/dev/null; then
+  echo "python is not installed"
+  apt install -y python3 python3-pip
+fi
+
+echo "Installing application"
+chmod +x dist
+
+echo "Verify if task management folder exists"
+if ! [ -d "$task_management_folder" ]; then
+  mkdir "$task_management_folder"
+fi
+
+
+chmod +x /usr/local/share/task-management
+
+echo "Verify if icon folder exists"
+if ! [ -d "$task_management_folder/icon/" ]; then
+  mkdir "$task_management_folder/icon/"
+fi
+
+
+cp dist/task-management /usr/local/bin/task-management
+cp dist/task.png /usr/local/share/task-management/icon/icon.png
+
+chmod +x /usr/local/bin/task-management
+
+echo "Creating a desktop icon"
+
+if [ -e "$desktop_file_icon/task-management.desktop" ]; then
+  rm "$desktop_file_icon/task-management.desktop"
+fi
+
+touch "$desktop_file_icon/task-management.desktop"
+echo "[Desktop Entry]" >> "$desktop_file_icon/task-management.desktop"
+echo "Version=1.0" >> "$desktop_file_icon/task-management.desktop"
+echo "Name=Task Management" >> "$desktop_file_icon/task-management.desktop"
+echo "Exec=/usr/local/bin/task-management" >> "$desktop_file_icon/task-management.desktop"
+echo "Icon=/usr/local/share/task-management/icon/icon.png" >> "$desktop_file_icon/task-management.desktop"
+echo "Type=Application" >> "$desktop_file_icon/task-management.desktop"
+echo "Terminal=false" >> "$desktop_file_icon/task-management.desktop"
+
+echo "Updating icons database"
+update-desktop-database "$desktop_file_icon"
+ 
+echo "Installed with success"
