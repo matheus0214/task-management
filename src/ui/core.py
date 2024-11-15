0090f45bb73fe9a6554bc840c803be0380692e20
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-15 14:56:54 -0300

    feat: create a window with min width and height

--- src/ui/core.py
+++ src/ui/core.py
@@ -1,8 +1,12 @@
 from PySide6.QtWidgets import QMainWindow
 
+from constants.core import WINDOW_MIN_HEIGHT, WINDOW_MIN_WIDTH
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Task Management")
-
+        self.setMinimumWidth(WINDOW_MIN_WIDTH)
+        self.setMinimumHeight(WINDOW_MIN_HEIGHT)
