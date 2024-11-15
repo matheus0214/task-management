36d837e1185ea77196d7f5ab7ac2f1fe69710691
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-15 16:01:38 -0300

    feat: add input text and button

--- src/ui/core.py
+++ src/ui/core.py
@@ -1,6 +1,7 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QGridLayout, QHBoxLayout, QMainWindow, QPushButton, QWidget
 
 from constants.core import WINDOW_MIN_HEIGHT, WINDOW_MIN_WIDTH
+from ui.input_text_widget import InputTextWidget
 
 
 class MainWindow(QMainWindow):
@@ -10,3 +11,19 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Task Management")
         self.setMinimumWidth(WINDOW_MIN_WIDTH)
         self.setMinimumHeight(WINDOW_MIN_HEIGHT)
+        
+        main_layout = QGridLayout()
+        layout_input_actions = QHBoxLayout()
+        btn = QPushButton("Register")
+
+        input_text_widget = InputTextWidget()
+
+        layout_input_actions.addWidget(input_text_widget)
+        layout_input_actions.addWidget(btn)
+
+        main_layout.addLayout(layout_input_actions, 0, 0)
+
+        widget = QWidget()
+        widget.setLayout(main_layout)
+
+        self.setCentralWidget(widget)
---
+++ src/ui/input_text_widget.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QLineEdit
+
+
+class InputTextWidget(QLineEdit):
+    def __init__(self):
+        super().__init__()
+
+
+
