4594a6fa0043eeb19a7e2a89c3063eede39badd2
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-16 11:17:31 -0300

    refactor: ajust align
    set all itens top stay on the top

--- src/ui/core.py
+++ src/ui/core.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QHBoxLayout, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 from constants.core import WINDOW_MIN_HEIGHT, WINDOW_MIN_WIDTH
 from ui.input_text_widget import InputTextWidget
@@ -15,6 +15,20 @@ class MainWindow(QMainWindow):
         
         main_layout = QVBoxLayout()
         layout_input_actions = QHBoxLayout()
+        layout_input_actions_widget = QWidget()
+
+        layout_input_actions_widget.setLayout(layout_input_actions)
+        layout_input_actions_widget.setSizePolicy(
+            layout_input_actions_widget.sizePolicy().horizontalPolicy(),
+            layout_input_actions_widget.sizePolicy().Policy.Minimum
+        )
+                
+        layout_tasks = QVBoxLayout()
+        layout_tasks.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        layout_tasks_widget = QWidget()
+        layout_tasks_widget.setLayout(layout_tasks)
+
         btn = QPushButton("Register")
         btn.pressed.connect(self.register_button_clicked)
 
@@ -23,7 +37,16 @@ class MainWindow(QMainWindow):
         layout_input_actions.addWidget(self.input_text_widget)
         layout_input_actions.addWidget(btn)
 
-        main_layout.addLayout(layout_input_actions)
+        layout_input_actions_widget.setMaximumHeight(
+            layout_input_actions.sizeHint().height()
+        )
+
+        list_task = ["Items", "itens 2"]
+        for iten in list_task:
+            layout_tasks.addWidget(QLabel(iten))
+
+        main_layout.addWidget(layout_input_actions_widget)
+        main_layout.addWidget(layout_tasks_widget) 
 
         widget = QWidget()
         widget.setLayout(main_layout)
@@ -31,6 +54,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
         main_layout.setAlignment(layout_input_actions, Qt.AlignmentFlag.AlignTop)
+        main_layout.setAlignment(layout_tasks, Qt.AlignmentFlag.AlignTop)
 
     def register_button_clicked(self):
         print("Click", self.input_text_widget.text())
