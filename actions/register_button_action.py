07860bb92fd16b430400e38f51b26c5f65f10b4c
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-16 17:49:23 -0300

    refactor: remove tkinter files

--- actions/register_button_action.py
+++
@@ -1,9 +0,0 @@
-class ButtonAction:
-    def __init__(self, tasks, tasks_list_widget) -> None:
-        self.tasks = tasks
-        self.tasks_list_wg =  tasks_list_widget
-
-    def register(self, input_label):
-        task = input_label.get("1.0", "end-1c") 
-        self.tasks.add(task)
-        self.tasks_list_wg.draw(task)
