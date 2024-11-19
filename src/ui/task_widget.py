2b8ba4b838f2087d1dfc409d1cdd695d1cb46eca
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-19 11:12:28 -0300

    feat: set icons
    change names 'Finishe' and 'Remove' in button show icons

--- src/ui/task_widget.py
+++ src/ui/task_widget.py
@@ -1,11 +1,14 @@
+import os
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QHBoxLayout, QLabel, QPushButton, QWidget
 
 from data.register import TaskRegisters
 
 
 class TaskWidget(QWidget):
-    def __init__(self, task: str, task_register: TaskRegisters,
-                 remove_from_view) -> None:
+    def __init__(
+        self, task: str, task_register: TaskRegisters, remove_from_view
+    ) -> None:
         super().__init__()
 
         self.task_register = task_register
@@ -13,13 +16,19 @@ class TaskWidget(QWidget):
 
         layout = QHBoxLayout()
         label = QLabel(task)
-        btn_finishe = QPushButton("Finishe")
-        btn_finishe.setMaximumWidth(60)
-        btn_finishe.clicked.connect(lambda  : self.finishe_task(task))
+        btn_finishe = QPushButton(
+            self.__get_icon("check-mark.png"),
+            "",
+        )
+        btn_finishe.setStyleSheet("border: 0")
 
-        btn_remove = QPushButton("Remove")
+        btn_finishe.setMaximumWidth(60)
+        btn_finishe.clicked.connect(lambda: self.finishe_task(task))
+
+        btn_remove = QPushButton(self.__get_icon("delete.png"), "")
         btn_remove.setMaximumWidth(60)
-        btn_remove.clicked.connect(lambda  : self.remove_task(task))
+        btn_remove.setStyleSheet("border: 0")
+        btn_remove.clicked.connect(lambda: self.remove_task(task))
 
         layout.addWidget(label)
         layout.addWidget(btn_finishe)
@@ -34,3 +43,6 @@ class TaskWidget(QWidget):
     def finishe_task(self, task: str) -> None:
         self.task_register.finishe(task)
         self.remove_from_view(task)
+
+    def __get_icon(self, icon_name: str) -> QIcon:
+        return QIcon(os.path.join(os.path.dirname(__file__), f"../images/{icon_name}"))
